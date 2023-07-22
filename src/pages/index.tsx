ad582e9a43c93629dcc66c63e825efce588b6ba9
akugone <dev@akumaw.com>
2023-07-23 01:08:42 +0200

    fix minor bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -119,25 +119,7 @@ function Home() {
 
                     <a
                       href='javascript:void(0)'
-                      className='glightbox mb-6 inline-flex items-center py-4 text-black hover:text-primary '>
-                      <span className='mr-[12px] flex h-[56px] w-[56px] items-center justify-center rounded-full border-2 border-current'>
-                        <svg
-                          width='14'
-                          height='16'
-                          viewBox='0 0 14 16'
-                          fill='none'
-                          xmlns='http://www.w3.org/2000/svg'>
-                          <path
-                            d='M13.5 7.06367C14.1667 7.44857 14.1667 8.41082 13.5 8.79572L1.5 15.7239C0.833334 16.1088 -3.3649e-08 15.6277 0 14.8579L6.05683e-07 1.00149C6.39332e-07 0.231693 0.833334 -0.249434 1.5 0.135466L13.5 7.06367Z'
-                            fill='currentColor'
-                          />
-                        </svg>
-                      </span>
-                      <span className='text-base font-medium'>
-                        <span className='block text-sm'> Watch Demo </span>
-                        See how it works
-                      </span>
-                    </a>
+                      className='glightbox mb-6 inline-flex items-center py-4 text-black hover:text-primary '></a>
                   </div>
                 </div>
               </div>
@@ -560,8 +542,7 @@ function Home() {
                       resume building where trust and security go hand in hand.
                       <br /> Ready to build your resume with confidence? Sign up for Trustky now and
                       unlock the power of privacy in the world of resume building. Your privacy
-                      matters, and at Trustky, we've got your back every step of the way. Réduire
-                      message.txt 3 Ko
+                      matters, and at Trustky, we've got your back every step of the way.
                     </p>
                   </div>
                 </div>
