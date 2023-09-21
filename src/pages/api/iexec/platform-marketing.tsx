b498d4344ba5d85fe075a677475d1503249c0e08
Quentin D.C <quent043@hotmail.com>
2023-09-21 06:51:17 +0100

    PR Comments - 6th batch
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/pages/api/iexec/platform-marketing.tsx
+++ src/pages/api/iexec/platform-marketing.tsx
@@ -1,59 +1,66 @@
-import { EmailType } from '../../../types';
+import { IUser } from '../../../types';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { sendMailToAddresses } from '../../../scripts/iexec/sendMailToAddresses';
-import { getUserWeb3mailPreferences } from '../../../queries/users';
-import { IExecWeb3mail, getWeb3Provider as getMailProvider, Contact } from '@iexec/web3mail';
+import { getUsersForWeb3MailFeature } from '../../../queries/users';
+import { Contact } from '@iexec/web3mail';
+import { generateWeb3mailProviders, prepareNonCronApi } from '../utils/web3mail';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const chainId = process.env.NEXT_PUBLIC_DEFAULT_CHAIN_ID;
   const platformId = process.env.NEXT_PUBLIC_PLATFORM_ID;
-  const privateKey = process.env.NEXT_PUBLIC_WEB3MAIL_PLATFORM_PRIVATE_KEY;
-  let successCount = 0,
-    errorCount = 0;
+  const securityKey = req.query.key as string;
+  const privateKey = process.env.NEXT_PUBLIC_WEB3MAIL_PLATFORM_PRIVATE_KEY as string;
 
-  if (!chainId) {
-    return res.status(500).json('Chain Id is not set');
-  }
+  let sentEmails = 0,
+    nonSentEmails = 0;
+
+  prepareNonCronApi(chainId, platformId, securityKey, privateKey, res);
+
   const { emailSubject, emailContent } = req.body;
   if (!emailSubject || !emailContent) return res.status(500).json(`Missing argument`);
-
-  if (!privateKey) {
-    return res.status(500).json('Private key is not set');
-  }
-
-  if (!platformId) {
-    return res.status(500).json('Platform Id is not set');
-  }
-
   try {
-    const mailWeb3Provider = getMailProvider(privateKey);
-    const web3mail = new IExecWeb3mail(mailWeb3Provider);
+    const { dataProtector, web3mail } = generateWeb3mailProviders(privateKey);
+
     const contactList: Contact[] = await web3mail.fetchMyContacts();
 
-    if (contactList) {
-      for (const contact of contactList) {
-        console.log(contact.address);
-        // Check whether the user opted for the called feature
-        //TODO loop sur les contact qui ont true en metadata
-        const userData = await getUserWeb3mailPreferences(
-          Number(chainId),
-          contact.address,
-          'activeOnPlatformMarketing',
-        );
-        if (!userData?.description?.web3mailPreferences?.activeOnPlatformMarketing) {
-          console.error(`User has not opted in for the ${EmailType.PlatformMarketing} feature`);
-          continue;
-        }
-        try {
-          //TODO Sortir ca de la loop , param = array address
-          await sendMailToAddresses(`${emailSubject}`, `${emailContent}`, [contact.address], true);
-          successCount++;
-          console.log('Email sent');
-        } catch (e: any) {
-          errorCount++;
-          console.error(e.message);
-        }
-      }
+    if (contactList.length === 0) {
+      return res.status(200).json('No users granted access to their email');
+    }
+
+    // This array has all the addresses of the users that have granted access to their email to this platform
+    const contactAddresses = contactList.map(contact => contact.address);
+
+    // TODO: Not tested
+    const response = await getUsersForWeb3MailFeature(
+      Number(chainId),
+      contactAddresses,
+      'activeOnPlatformMarketing',
+    );
+
+    // This array has all the users that have granted access to their email to this platform and opted for the platform marketing feature
+    const users: IUser[] = response.data.data.users;
+    if (users.length === 0) {
+      return res.status(200).json(`No User opted for this feature`);
+    }
+
+    if (users) {
+      const usersAddresses = users.map(user => user.address);
+      // // TODO: Remove for prod
+      // const usersAddresses = [
+      //   '0x89de0dd433bd1f6a4ec5dd2593f44b9dd4fad2f0',
+      //   '0x86a86da3e48905f0ecbf788948ada2789669ee11',
+      //   '0x3D0c6A35DcD9AEB46b493cd6CC9Ace84583b7aE8',
+      // ];
+      const { successCount, errorCount } = await sendMailToAddresses(
+        `${emailSubject}`,
+        `${emailContent}`,
+        usersAddresses,
+        false,
+        dataProtector,
+        web3mail,
+      );
+      sentEmails += successCount;
+      nonSentEmails += errorCount;
     }
   } catch (e: any) {
     console.error(e);
@@ -61,5 +68,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
   return res
     .status(200)
-    .json(`Web3 Emails sent - ${successCount} email successfully sent | ${errorCount} errors`);
+    .json(
+      `Web3 Emails sent - ${sentEmails} email successfully sent | ${nonSentEmails} non sent emails`,
+    );
 }
