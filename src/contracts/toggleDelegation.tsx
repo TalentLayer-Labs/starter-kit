c70f89a9f27f23509e1504955b2536238d873e76
Gathin <gathint23@gmail.com>
2023-09-22 23:23:05 +0530

    Fix: Delegation Bugs on Wagmi Update

--- src/contracts/toggleDelegation.tsx
+++ src/contracts/toggleDelegation.tsx
@@ -1,30 +1,29 @@
 import { createMultiStepsTransactionToast, showErrorTransactionToast } from '../utils/toast';
-import { PublicClient, WalletClient } from 'viem';
+import { Address, PublicClient, WalletClient } from 'viem';
 import TalentLayerID from './ABI/TalentLayerID.json';
-import { useConfig } from '../hooks/useConfig';
-import { usePublicClient } from 'wagmi';
 
 export const toggleDelegation = async (
   chainId: number,
   user: string,
+  config: any,
   DelegateAddress: string,
   publicClient: PublicClient,
   walletClient: WalletClient,
   validateState: boolean,
+  
 ): Promise<void> => {
   try {
-    let tx: any;
+    let tx: Address;
     let toastMessages;
-    const config = useConfig();
-
     if (validateState === true) {
       const { request } = await publicClient.simulateContract({
         address: config.contracts.talentLayerId,
         abi: TalentLayerID.abi,
         functionName: 'addDelegate',
         args: [user, DelegateAddress],
+        account: walletClient.account?.address,
       });
-      const tx = await walletClient.writeContract(request);
+      tx = await walletClient.writeContract(request);
       toastMessages = {
         pending: 'Submitting the delegation...',
         success: 'Congrats! the delegation is active',
@@ -36,19 +35,20 @@ export const toggleDelegation = async (
         abi: TalentLayerID.abi,
         functionName: 'removeDelegate',
         args: [user, DelegateAddress],
-      });
-      const tx = await walletClient.writeContract(request);
+        account: walletClient.account?.address,
+      }); 
+      tx = await walletClient.writeContract(request);
       toastMessages = {
         pending: 'Canceling the delegation...',
         success: 'The delegation has been canceled',
         error: 'An error occurred while canceling the delegation',
       };
     }
-
+    
     await createMultiStepsTransactionToast(
       chainId,
       toastMessages,
-      usePublicClient({ chainId }),
+      publicClient,
       tx,
       'Delegation',
     );
