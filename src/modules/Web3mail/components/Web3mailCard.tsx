48912647781b94b8eb091b6d9baf5d87a5af461e
Romain <romain@talentlayer.org>
2023-09-23 15:11:03 +0200

    Update src/modules/Web3mail/components/Web3mailCard.tsx
    Co-authored-by: Quentin <44470601+quent043@users.noreply.github.com>

--- src/modules/Web3mail/components/Web3mailCard.tsx
+++ src/modules/Web3mail/components/Web3mailCard.tsx
@@ -88,7 +88,7 @@ function Web3mailCard() {
             <span>
               {' '}
               Your email will never be stored in a database, with iExec web3mail, you can allow your
-              email to be used while it will never be known by us{' '}
+              email to be used without being disclosed to anyone, including us{' '}
             </span>
           </p>
           <div className='mt-3'>
