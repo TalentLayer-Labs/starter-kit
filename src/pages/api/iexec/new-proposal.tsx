367ca0a7af6e3a3847c0fbcf4522cc3f33b6e3fd
Quentin D.C <quent043@hotmail.com>
2023-09-23 18:49:08 +0100

    Fixed condition mistake
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/pages/api/iexec/new-proposal.tsx
+++ src/pages/api/iexec/new-proposal.tsx
@@ -85,7 +85,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         validUserAddresses.includes(proposal.service.buyer.address);
       });
 
-      if (proposalEmailsToBeSent.length > 0) {
+      if (proposalEmailsToBeSent.length === 0) {
         return res
           .status(200)
           .json(
