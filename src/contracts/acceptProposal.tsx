fdb520211b8f673c6c59cf4d34a27ada7e06361a
pranavsinghal <pranavsinghal96@gmail.com>
2023-10-05 20:32:44 +0530

    added escrow approval with token approval in sdk

--- src/contracts/acceptProposal.tsx
+++ src/contracts/acceptProposal.tsx
@@ -22,9 +22,6 @@ export const validateProposal = async (
 ): Promise<void> => {
   const config = getConfig(chainId);
 
-  const _balance: any = await tlClient.erc20.balanceOf(rateToken);
-  
-  console.log({_balance})
   try {
     if (rateToken === ZERO_ADDRESS) {
 
@@ -48,38 +45,6 @@ export const validateProposal = async (
         throw new Error('Approve Transaction failed');
       }
     } else {
-
-      const [_address] = await walletClient.getAddresses();
-
-      const allowance: any = await publicClient.readContract({
-        address: rateToken,
-        abi: ERC20.abi,
-        functionName: 'allowance',
-        args: [_address, config.contracts.talentLayerEscrow],
-      });
-
-      if (allowance < value) {
-
-        const tx1 = await tlClient.erc20.approve(rateToken, value);
-
-        const receipt1 = await toast.promise(
-          publicClient.waitForTransactionReceipt({ hash: tx1 }),
-          {
-            pending: {
-              render() {
-                return (
-                  <TransactionToast message='Your approval is in progress' transactionHash={tx1} />
-                );
-              },
-            },
-            success: 'Transaction validated',
-            error: 'An error occurred while updating your profile',
-          },
-        );
-        if (receipt1.status !== 'success') {
-          throw new Error('Approve Transaction failed');
-        }
-      }
 
       const { tx: tx2 } = await tlClient.escrow.approve(serviceId, proposalId, metaEvidenceCid);
       const receipt2 = await toast.promise(publicClient.waitForTransactionReceipt({ hash: tx2 }), {
