4661b3192ead62de7cd8bbfd7281b7a9401f8b1d
Dercio <contact@creedscode.cc>
2023-10-11 20:37:38 +0200

    hotfix or so

--- src/pages/landing/onboarding.tsx
+++ src/pages/landing/onboarding.tsx
@@ -2,59 +2,36 @@ import * as Yup from 'yup'
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useCreateSpaceMutation } from "../../modules/MultiDomain/hooks/UseCreateSpaceMutation";
 import { generateSubdomainPrefix } from "../../modules/MultiDomain/utils";
+import { useState } from 'react';
 
-interface IFormValues {
-  name: string
-}
-
-const validationSchema = Yup.object({
-  name: Yup.string().required('Please provide an name for your space'),
-});
-
-function Onboarding() {
+function onboarding() {
   const { data: createdSpace, mutateAsync: createSpaceAsync } = useCreateSpaceMutation();
-
-  const initialValues: IFormValues = {
-    name: '',
-  };
+  const [name, setName] = useState("");
 
   const onSubmit = async (
-    values: IFormValues,
-    {
-      setSubmitting,
-      resetForm,
-    }: { setSubmitting: (isSubmitting: boolean) => void; resetForm: () => void },
   ) => {
-    setSubmitting(false);
-    const subdomainPrefix = generateSubdomainPrefix(values.name);
+    const subdomainPrefix = generateSubdomainPrefix(name);
     const subdomain = `${subdomainPrefix}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`;
-    await createSpaceAsync({ subdomain: subdomain, name: values.name, primaryColor: "#ffffff", secondaryColor: "#ffffff" });
+    await createSpaceAsync({ subdomain: subdomain, name: name, primaryColor: "#ffffff", secondaryColor: "#ffffff" });
     window.location.href = `${window.location.protocol}//${subdomain}/admin`;
-    resetForm();
   }
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-      <Form>
-        <div className='flex'>
-          <label className='block flex-1 mr-4'>
-            <span className='text-gray-100'>Name</span>
-            <Field
-              type='string'
-              id='name'
-              name='name'
-              className='mt-1 mb-1 block w-full rounded-xl border border-gray-700 bg-midnight shadow-sm focus:ring-opacity-50'
-              placeholder=''
-            />
-            <span className='text-red-500'>
-              <ErrorMessage name='name' />
-            </span>
-          </label>
-        </div>
-        <button type="submit">Go</button>
-      </Form >
-    </Formik >
+    <>
+      <div className='flex'>
+        <label className='block flex-1 mr-4'>
+          <label htmlFor="custom-domain" className='text-gray-100'>Name</label>
+          <input
+            type='string'
+            id='name'
+            className='mt-1 mb-1 block w-full rounded-xl border border-gray-700 bg-midnight shadow-sm focus:ring-opacity-50'
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+      </div>
+      <button onClick={onSubmit}>Go</button>
+    </>
   );
 }
 
-export default Onboarding;
+export default onboarding;
