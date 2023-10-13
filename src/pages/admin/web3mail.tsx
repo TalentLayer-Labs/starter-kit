f5453ba356e9f2b235207762b2562d04c6809ead
Quentin D.C <quent043@hotmail.com>
2023-10-13 15:07:06 +0100

    Modified Text
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/pages/admin/web3mail.tsx
+++ src/pages/admin/web3mail.tsx
@@ -25,7 +25,7 @@ function Web3mail() {
     <div className='max-w-7xl mx-auto text-gray-200 sm:px-4 lg:px-0'>
       <div className=' -mx-6 -mt-6 '>
         <div className='flex py-2 px-6 items-center border-b w-full border-gray-700 mb-8'>
-          <p className='text-2xl font-medium flex-1'>Send a Web3Mail</p>
+          <p className='text-2xl font-medium flex-1'>Send Web3 Mails</p>
         </div>
       </div>
       <ContactListForm userDetailList={contactList} usersLoaded={contactsLoaded} />
