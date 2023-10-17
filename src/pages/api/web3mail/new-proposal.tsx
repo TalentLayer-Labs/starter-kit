41718016a62f8e3e0213047c3dad4ff306647bf9
0xRomain <romain@talentlayer.org>
2023-10-17 15:44:46 +0200

    change method to increase max duration time

--- src/pages/api/web3mail/new-proposal.tsx
+++ src/pages/api/web3mail/new-proposal.tsx
@@ -13,6 +13,8 @@ import {
 import { generateWeb3mailProviders, getValidUsers, prepareCronApi } from '../utils/web3mail';
 import { renderTokenAmount } from '../../../utils/conversion';
 
+export const maxDuration = 300;
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const chainId = process.env.NEXT_PUBLIC_DEFAULT_CHAIN_ID as string;
   const platformId = process.env.NEXT_PUBLIC_PLATFORM_ID as string;
