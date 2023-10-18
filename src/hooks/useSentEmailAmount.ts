c6e477d0f8545c8b0abf03f4185bc0f40397b1a8
Quentin D.C <quent043@hotmail.com>
2023-10-18 10:44:15 +0100

    Removed logs
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/hooks/useSentEmailAmount.ts
+++ src/hooks/useSentEmailAmount.ts
@@ -1,15 +1,14 @@
 import { useEffect, useState } from 'react';
 import { fetchEmailAmount } from '../components/request';
 
-const useSentEmailAmount = (): { sentEmailsNumber: number | undefined } => {
-  const [sentEmailsNumber, setSentEmailAmount] = useState<number>();
+const useSentEmailAmount = (): { sentEmailsNumber: number } => {
+  const [sentEmailsNumber, setSentEmailAmount] = useState<number>(0);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetchEmailAmount();
         const emailsNumber: number = response?.data?.data;
-        console.log('emailsNumber', emailsNumber);
         setSentEmailAmount(emailsNumber);
       } catch (error: any) {
         // eslint-disable-next-line no-console
