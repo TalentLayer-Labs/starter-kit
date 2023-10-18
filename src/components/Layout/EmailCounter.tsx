3c172e648df7fde7a3782e01093d8ed410d4154a
Quentin D.C <quent043@hotmail.com>
2023-10-18 10:42:19 +0100

    Added fun rendering
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/components/Layout/EmailCounter.tsx
+++ src/components/Layout/EmailCounter.tsx
@@ -1,14 +1,24 @@
 import useSentEmailAmount from '../../hooks/useSentEmailAmount';
 
+const renderEmailNumber = (sentEmailsNumber: number) => {
+  if (sentEmailsNumber < 150)
+    return <span className='text-xl font-bold text-red-500'>{sentEmailsNumber}</span>;
+  if (sentEmailsNumber >= 150 && sentEmailsNumber < 350)
+    return <span className='text-xl font-bold text-yellow-500'>{sentEmailsNumber}</span>;
+  if (sentEmailsNumber >= 350 && sentEmailsNumber < 499)
+    return <span className='text-xl font-bold text-green-500'>{sentEmailsNumber}</span>;
+  if (sentEmailsNumber >= 500)
+    return <span className='text-xl font-bold text-green-500'>{sentEmailsNumber}</span>;
+};
+
 const emailCounter = () => {
   const { sentEmailsNumber } = useSentEmailAmount();
-  console.log('sentEmailsNumber', sentEmailsNumber);
   return (
     <>
       <p>
         <span className='text-s text-gray-200'>Sent Emails : </span>
-        <span className='text-xl font-bold text-gray-500'>{sentEmailsNumber}</span>
-        /500
+        {renderEmailNumber(sentEmailsNumber)}
+        <span className='text-s text-gray-200'>/500 {sentEmailsNumber >= 500 ? '🥳' : ''}</span>
       </p>
     </>
   );
