2742c13e5b9fb3e7ff6b9930394e2e958eeab86c
Romain <romain@talentlayer.org>
2023-10-23 09:11:42 +0200

    update mongo env

--- src/mongo/mongodb.ts
+++ src/mongo/mongodb.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose"
 
-const { NEXT_PRIVATE_DATABASE_URI } = process.env
+const { NEXT_MONGO_URI } = process.env
 
 export const connection = async () => {
   const conn = await mongoose
-    .connect(NEXT_PRIVATE_DATABASE_URI as string)
+    .connect(NEXT_MONGO_URI as string)
     .catch(err => console.log(err))
   console.log("Mongoose Connection Established")
   return { conn }
