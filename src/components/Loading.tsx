f12034a4701095552403aed045ac652ae01c5512
Romain <romain@talentlayer.org>
2023-10-26 15:54:01 +0200

    handle margin on parent

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,6 @@
 function Loading() {
   return (
-    <div className='flex justify-center items-center mt-8'>
+    <div className='flex justify-center items-center'>
       <div
         className='inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-redpraha border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]'
         role='status'>
