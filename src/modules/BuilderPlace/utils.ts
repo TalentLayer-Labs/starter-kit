d7d184d274901b27a2872b13407e9c0b844189e1
Quentin D.C <quent043@hotmail.com>
2023-10-27 12:18:10 +0100

    Added '-' in subdomain utils function
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/modules/BuilderPlace/utils.ts
+++ src/modules/BuilderPlace/utils.ts
@@ -1,6 +1,6 @@
 export function generateSubdomainPrefix(name: string): string {
   // Remove any non-alphanumeric characters and replace spaces with hyphens
-  const alphanumericName = name.replace(/[^a-zA-Z0-9 ]/g, '').replace(/\s+/g, '-');
+  const alphanumericName = name.replace(/[^a-zA-Z0-9\- ]/g, '').replace(/\s+/g, '-');
   // Convert to lowercase and truncate to 63 characters (the maximum length for a subdomain)
   return alphanumericName.toLowerCase().substring(0, 63);
 }
