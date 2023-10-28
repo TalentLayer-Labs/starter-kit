9ffb35cc6fc779f7c66105d137fa368cb8ae3fad
Quentin D.C <quent043@hotmail.com>
2023-10-28 11:40:29 +0100

    added placeholder
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/pages/landing/onboarding/step3.tsx
+++ src/pages/landing/onboarding/step3.tsx
@@ -94,7 +94,7 @@ function onboardingStep3() {
                     id='subdomain'
                     name='subdomain'
                     className='mt-1 mb-1 block w-full rounded-xl border border-redpraha bg-midnight shadow-sm focus:ring-opacity-50'
-                    placeholder=''
+                    placeholder={`.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`}
                   />
                 </label>
                 <span className='text-red-500'>
