28b3df234a1c62d035c355239a84419174f31822
Romain <romain@talentlayer.org>
2023-11-07 15:51:17 +0100

    fix start url

--- src/pages/api/manifest.ts
+++ src/pages/api/manifest.ts
@@ -1,10 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getBuilderPlace } from '../../modules/BuilderPlace/queries';
+import { IBuilderPlace } from '../../modules/BuilderPlace/types';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const host = req.headers.host as string;
   const data = await getBuilderPlace(host);
-  const builderPlace = data?.props?.builderPlace;
+  const builderPlace = data?.props?.builderPlace as IBuilderPlace;
 
   try {
     if (!builderPlace) throw new Error('Builder Place not found');
@@ -29,7 +30,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           sizes: '512x512',
         },
       ],
-      start_url: '.',
+      start_url: `${builderPlace.customDomain || builderPlace.subdomain}`,
       display: 'standalone',
       theme_color: builderPlace?.palette?.primary || '#000000',
       background_color: builderPlace?.palette?.primaryContent || '#ffffff',
