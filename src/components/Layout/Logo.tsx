5d3b430a8b33680bcb4bbf35e71816f216dbaa9b
Romain <romain@talentlayer.org>
2023-11-16 16:36:38 +0300

    dynamic logo

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -1,23 +1,20 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import { useContext } from 'react';
 import BuilderPlaceContext from '../../modules/BuilderPlace/context/BuilderPlaceContext';
 
-function Logo({ theme = 'dark' }: { theme?: 'dark' | 'light' }) {
+function Logo() {
   const { builderPlace } = useContext(BuilderPlaceContext);
 
   return (
     <div>
       <Link href='/' className='flex items-end flex-wrap pt-2 pb-2 sm:pt-0 sm:pb-0'>
-        <h1 className='text-base-content text-3xl font-bold'>{builderPlace?.name}</h1>
-
-        {/* <Image
-          src={'/logo-text-base-content.png'}
-          width={180}
-          height={36}
-          alt='BuilderPlace logo'
-          className='-ml-2 sm:ml-0'
-        /> */}
+        <h1 className='text-base-content text-3xl font-bold'>
+          {builderPlace?.logo ? (
+            <img src={builderPlace?.logo} alt={builderPlace.name} className='' />
+          ) : (
+            builderPlace?.name
+          )}
+        </h1>
       </Link>
     </div>
   );
