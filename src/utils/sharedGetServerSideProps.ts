4dd120685fa760a29ca913cbfd35c109ba3b8da6
Romain <romain@talentlayer.org>
2023-12-15 16:36:32 +0100

    increase cache duration

--- src/utils/sharedGetServerSideProps.ts
+++ src/utils/sharedGetServerSideProps.ts
@@ -6,7 +6,7 @@ export const sharedGetServerSideProps: GetServerSideProps = async (
   customLogic?: (ctx: GetServerSidePropsContext) => Promise<Record<string, any>>,
 ): Promise<GetServerSidePropsResult<any>> => {
   const { req, res, params } = context;
-  res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59');
+  res.setHeader('Cache-Control', 'public, s-maxage=30, stale-while-revalidate=600');
   console.log('sharedGetServerSideProps', params, res, req);
   const builderPlace = await getBuilderPlace(params?.domain as string);
 
