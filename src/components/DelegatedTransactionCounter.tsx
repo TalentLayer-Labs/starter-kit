3cbc959c938dcb01ba5355fd3a6ff08cc683a665
Romain <romain@talentlayer.org>
2023-12-29 11:18:35 +0100

    renaming

--- src/components/DelegatedTransactionCounter.tsx
+++ src/components/DelegatedTransactionCounter.tsx
@@ -2,14 +2,15 @@ import { useContext } from 'react';
 import TalentLayerContext from '../context/talentLayer';
 import { MAX_TRANSACTION_AMOUNT } from '../config';
 
-const renderTxNumber = (sentEmailsNumber: number) => {
-  if (sentEmailsNumber < 20)
-    return <span className='text-xs text-green-500'>{sentEmailsNumber}</span>;
-  if (sentEmailsNumber >= 20 && sentEmailsNumber < 40)
-    return <span className='text-xs text-yellow-500'>{sentEmailsNumber}</span>;
-  if (sentEmailsNumber >= 40 && sentEmailsNumber <= MAX_TRANSACTION_AMOUNT)
-    return <span className='text-xs text-red-500'>{sentEmailsNumber}</span>;
+const renderTxNumber = (sentTransactionsNumber: number) => {
+  if (sentTransactionsNumber < 20)
+    return <span className='text-xs text-green-500'>{sentTransactionsNumber}</span>;
+  if (sentTransactionsNumber >= 20 && sentTransactionsNumber < 40)
+    return <span className='text-xs text-yellow-500'>{sentTransactionsNumber}</span>;
+  if (sentTransactionsNumber >= 40 && sentTransactionsNumber <= MAX_TRANSACTION_AMOUNT)
+    return <span className='text-xs text-red-500'>{sentTransactionsNumber}</span>;
 };
+
 const renderWaitPeriod = (lastTxTime: number) => {
   const oneWeekAgoMilliseconds = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000).getTime();
   const differenceInMilliseconds = lastTxTime - oneWeekAgoMilliseconds;
@@ -28,10 +29,10 @@ const DelegatedTransactionCounter = () => {
         <>
           <p className='text-base-content mt-2'>
             <span className='text-xs'>Free Weekly Tx : </span>
-            {renderTxNumber(workerProfile?.weeklyTransactionCounter)}
+            {renderTxNumber(workerProfile?.weeklyTransactionCounter || 0)}
             <span className='text-xs'>/{MAX_TRANSACTION_AMOUNT}</span>
           </p>
-          {workerProfile?.weeklyTransactionCounter === MAX_TRANSACTION_AMOUNT && (
+          {(workerProfile?.weeklyTransactionCounter || 0) === MAX_TRANSACTION_AMOUNT && (
             <>
               <p className='text-base-content text-xs'>Max tx reached, please wait</p>
               {workerProfile?.counterStartDate && renderWaitPeriod(workerProfile.counterStartDate)}
