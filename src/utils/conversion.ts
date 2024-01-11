e33eb239a7533d2de1527cb1c759361f90fb2ff4
Romain <romain@talentlayer.org>
2024-01-11 15:56:16 +0100

    fix edge case with invalid format

--- src/utils/conversion.ts
+++ src/utils/conversion.ts
@@ -2,6 +2,13 @@ import { formatUnits } from 'viem';
 import { IToken } from '../types';
 
 export const renderTokenAmount = (token: IToken, value: string): string => {
-  const formattedValue = formatUnits(BigInt(value), token.decimals);
+  console.log('renderTokenAmount', value, value.toString());
+
+  const parsedValue = Number(value);
+  if (isNaN(parsedValue)) {
+    return 'invalid format';
+  }
+
+  const formattedValue = formatUnits(BigInt(parsedValue), token.decimals);
   return `${formattedValue} ${token.symbol}`;
 };
